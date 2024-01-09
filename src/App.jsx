95a39ef30b66b33271a4c11c6a03ca1c8c63e695
sri-rishi <sririshi2612@gmail.com>
2024-01-09 23:39:47 +0530

    feat: added components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css';
+import { Details,Sidebar } from './components';
 
 function App() {
   return (
     <div className="App">
-      
+      <Sidebar />
+      <Details />
     </div>
   );
 }
