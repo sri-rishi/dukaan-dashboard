95bdd3d85596ef3d4e73052d711ec72f02051a9f
sri-rishi <sririshi2612@gmail.com>
2024-01-09 18:01:37 +0530

    imported and exported files

---
+++ src/assets/index.js
@@ -0,0 +1,36 @@
+import Logo from "./images/Image.png";
+import HomeSvg from "./images/home.svg";
+import OrderSvg from "./images/order.svg";
+import ProductSvg from "./images/products.svg";
+import DeliverySvg from "./images/delivery.svg";
+import MarketingSvg from "./images/marketing.svg";
+import AnalyticsSvg from "./images/analytics.svg";
+import PaymentsSvg from "./images/payments.svg";
+import ToolsSvg from "./images/tools.svg";
+import DiscountSvg from "./images/discounts.svg"
+import AudienceSvg from "./images/audience.svg";
+import AppearenceSvg from "./images/appearence.svg";
+import PluginsSvg from "./images/plugins.svg";
+
+import {
+    FaChevronDown,
+    GoQuestion
+} from "./icons";
+
+export {
+    Logo,
+    HomeSvg,
+    OrderSvg,
+    ProductSvg,
+    DeliverySvg,
+    MarketingSvg,
+    AnalyticsSvg,
+    PaymentsSvg,
+    ToolsSvg,
+    DiscountSvg,
+    AudienceSvg,
+    AppearenceSvg,
+    PluginsSvg,
+    FaChevronDown,
+    GoQuestion
+}
---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import Sidebar from "./sidebar/Sidebar";
+import Header from "./header/Header";
+
+export {
+    Sidebar,
+    Header
+}
